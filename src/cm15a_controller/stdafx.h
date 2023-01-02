2d49a871ad6fe70823f69236f46d92f4e51fb3a6
casey langen <casey.langen@gmail.com>
2023-01-02 15:07:20 -0800

    Consistent handling of WINVER macros, and update Windows project settings to be more consistent across all projects.

--- src/cm15a_controller/stdafx.h
+++ src/cm15a_controller/stdafx.h
@@ -1,22 +1,16 @@
 #pragma once
 
+#ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+    #define NOMINMAX
+#endif
 #ifndef WINVER
-#define WINVER 0x0502
+    #define WINVER 0x0601
+    #define _WIN32_WINNT 0x0601
 #endif
-
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0502
-#endif
-
-#ifndef _WIN32_WINDOWS
-#define _WIN32_WINDOWS 0x0410
-#endif
-
-#ifndef _WIN32_IE
-#define _WIN32_IE 0x0600
-#endif
-
-#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
 #include <windows.h>
 
